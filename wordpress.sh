a5bdcd8b6d260d6ea8fb5e43196ecb60a56738f4
xiaojiuling <64819785+xiaojiuling@users.noreply.github.com>
2023-04-23 09:43:41 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,8 +1,9 @@
 wget https://cn.wordpress.org/latest-zh_CN.tar.gz
 tar -xvf latest-zh_CN.tar.gz -C ./lamp/httpd/htdocs
 rm -rf ./lamp/httpd/htdocs/index && mv ./lamp/httpd/htdocs/wordpress/* ./lamp/httpd/htdocs
-docker exec lamp-mysql sh /mysql-create.sh
 docker exec lamp-php docker-php-ext-install mysqli
 docker restart lamp-php
 chmod 777 ./lamp/httpd/htdocs
 rm -rf ./latest-zh_CN.tar.gz
+sleep 5s
+docker exec lamp-mysql sh /mysql-create.sh
